fb487fa2c6407e578335f1059a906aa3c46513c7
Onyeriri Victor <viktorchinonso@gmail.com>
2023-03-05 08:40:12 +0000

    Added testing file

--- server/src/app.js
+++ server/src/app.js
@@ -16,13 +16,13 @@ app.use(
 app.use(morgan("combined"));
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "..", "public ")));
+app.use(express.static(path.join(__dirname, "..", "build")));
 
 app.use("/planets", planetRouter);
 app.use("/launches", launchesRouter);
 
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "public ", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "build", "index.html"));
 });
 
 module.exports = app;
