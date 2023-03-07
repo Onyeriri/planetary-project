2002b0e4b7c2e652e0a9a16bb674a283b78a361e
Onyeriri Victor <viktorchinonso@gmail.com>
2023-03-07 14:40:21 +0000

    Added pm2 for performance

--- node-performance/server.js
+++ node-performance/server.js
@@ -1,7 +1,8 @@
-const express = require("express");
 const cluster = require("cluster");
+const express = require("express");
+const os = require("os");
 
-const app = express();
+const numCPUs = os.cpus().length;
 
 function delay(duration) {
   const startTime = Date.now();
@@ -11,20 +12,28 @@ function delay(duration) {
   }
 }
 
-app.get("/", (req, res) => {
-  res.send(`Performance test!!!...${process.pid}`);
-});
-
-app.get("/timer", (req, res) => {
-  delay(9000);
-  res.send(`Ding ding... ${process.pid}`);
-});
+const app = express();
 
 if (cluster.isMaster) {
-  console.log("Master server started...");
-  cluster.fork();
-  cluster.fork();
+  console.log(`Master ${process.pid} is running`);
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+    console.log("Let's fork another worker!");
+    cluster.fork();
+  });
 } else {
-  console.log("Started worker server");
+  app.get("/", (req, res) => {
+    res.send(`Performance test!!!...${process.pid}`);
+  });
+
+  app.get("/timer", (req, res) => {
+    delay(9000);
+    res.send(`Ding ding... ${process.pid}`);
+  });
   app.listen(3000);
+  console.log(`Worker ${process.pid} started`);
 }
